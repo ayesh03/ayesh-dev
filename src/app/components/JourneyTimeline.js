5f817b1c6329e7c3030fa4772a714e692a58c5bb
Ayesh Ranjan Bhuyan <pearlvayesh@gmail.com>
2025-11-12 12:22:26 +0530

    Update JourneyTimeline.js

--- src/app/components/JourneyTimeline.js
+++ src/app/components/JourneyTimeline.js
@@ -24,6 +24,11 @@ const timeline = [
     date: 'Mar 2024 – May 2025',
     description: 'Joined as a full-time software developer, contributed to scalable web apps using React, Angular, Node.js, CI/CD, and testing.',
   }
+  {
+    title: '👨‍💻 Application Developer at Areca Embedded System Pvt Ltd',
+    date: 'July 2025 – Present',
+    description: 'Joined as a full-time software developer, contributed to scalable web apps using React, Angular, Node.js, CI/CD, and testing.',
+  }
 ];
 
 export default function JourneyTimeline({ onClose }) {
